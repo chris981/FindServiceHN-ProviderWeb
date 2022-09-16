b14d7627b581b91f1c79d1b9f43d2eb6298fe0be
chris981 <cdams_98@hotmail.com>
2022-09-15 21:11:14 -0600

    Update providers-eval.service.ts

--- src/app/shared/services/providers-eval.service.ts
+++ src/app/shared/services/providers-eval.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { environment } from 'src/environments/environment';
+import { ProviderEvalModel } from '../models/provider-eval-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProvidersEvalService {
 
-  constructor() { }
+  baseUrl = environment.baseUrl;
+  constructor(private http: HttpClient) { }
+  
+  GetAllProviderEval(): Observable<Array<ProviderEvalModel>> {
+    return this.http.get<Array<ProviderEvalModel>>(`${this.baseUrl}/api/DayHour/GetDayHours`);
+  }
+
+  DeleteProviderEval(id: number): Observable<boolean> {
+    return this.http.delete<boolean>(`${this.baseUrl}/api/DayHour/Remove/${id}`);
+  }
+
+  UpdateProviderEval(category: ProviderEvalModel): Observable<ProviderEvalModel> {
+    return this.http.put<ProviderEvalModel>(`${this.baseUrl}/api/DayHour/Update`, category);
+  }
 }
