b04f8879cb592c55c0c19c87b0a4b957710c85a1
chris981 <cdams_98@hotmail.com>
2022-09-15 21:10:09 -0600

    Update providers.service.ts

--- src/app/shared/services/providers.service.ts
+++ src/app/shared/services/providers.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { environment } from 'src/environments/environment';
+import { ProvidersModel } from '../models/providers-model';
 @Injectable({
   providedIn: 'root'
 })
 export class ProvidersService {
 
-  constructor() { }
+  baseUrl = environment.baseUrl;
+  constructor(private http: HttpClient) { }
+
+  GetAllProviders(): Observable<Array<ProvidersModel>> {
+    return this.http.get<Array<ProvidersModel>>(`${this.baseUrl}/api/DayHour/GetDayHours`);
+  }
+
+  DeleteProviders(id: number): Observable<boolean> {
+    return this.http.delete<boolean>(`${this.baseUrl}/api/DayHour/Remove/${id}`);
+  }
+
+  UpdateProviders(category: ProvidersModel): Observable<ProvidersModel> {
+    return this.http.put<ProvidersModel>(`${this.baseUrl}/api/DayHour/Update`, category);
+  }
 }
