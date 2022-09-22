c26c3c08ff79b4668364b6d66427e2f66f92d591
chris981 <cdams_98@hotmail.com>
2022-09-21 20:22:19 -0600

    Update providers-eval.service.ts

--- src/app/shared/services/providers-eval.service.ts
+++ src/app/shared/services/providers-eval.service.ts
@@ -13,14 +13,16 @@ export class ProvidersEvalService {
   constructor(private http: HttpClient) { }
   
   GetAllProviderEval(): Observable<Array<ProviderEvalModel>> {
-    return this.http.get<Array<ProviderEvalModel>>(`${this.baseUrl}/api/DayHour/GetDayHours`);
+    return this.http.get<Array<ProviderEvalModel>>(`${this.baseUrl}/api/ProvidersEval/GetProvidersEval`);
   }
-
+  CreateProvidersEval(providersEval: ProviderEvalModel): Observable<ProviderEvalModel> {
+    return this.http.post<ProviderEvalModel>(`${this.baseUrl}/api/ProvidersEval/Create`, providersEval);
+  }
   DeleteProviderEval(id: number): Observable<boolean> {
-    return this.http.delete<boolean>(`${this.baseUrl}/api/DayHour/Remove/${id}`);
+    return this.http.delete<boolean>(`${this.baseUrl}/api/ProvidersEval/Remove/${id}`);
   }
 
-  UpdateProviderEval(category: ProviderEvalModel): Observable<ProviderEvalModel> {
-    return this.http.put<ProviderEvalModel>(`${this.baseUrl}/api/DayHour/Update`, category);
+  UpdateProviderEval(providerEval: ProviderEvalModel): Observable<ProviderEvalModel> {
+    return this.http.put<ProviderEvalModel>(`${this.baseUrl}/api/ProvidersEval/Update`, providerEval);
   }
 }
